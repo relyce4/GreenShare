2058341a7313d2bc7db0249f3062104845ce296e
Christian <relyce4@gmail.com>
2021-12-20 17:01:48 +0100

    Initial commit

---
+++ src/main/java/com/greenshare/site/GreenShareApplication.java
@@ -0,0 +1,13 @@
+package com.greenshare.site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenShareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenShareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/greenshare/site/GreenShareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greenshare.site;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenShareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
